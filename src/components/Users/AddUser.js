bab6f772cfd1b65bf153ff0e7763f61d1be0d309
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-21 23:02:52 +0530

    added college filed as required field

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -13,10 +13,10 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 || enteredCollege.trim().length === 0) {
       setError({
         title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).',
+        message: 'Please enter a valid name ,age (non-empty values) and college.',
       });
       return;
     }
